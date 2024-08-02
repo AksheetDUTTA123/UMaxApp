1920231f43158f4c2157865930588f3257c253f2
Aksheet Dutta <ardutta23@gmail.com>
2024-08-02 13:20:41 -0400

    Initial Commit

---
+++ UMaxApp/UMaxAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UMaxAppApp.swift
+//  UMaxApp
+//
+//  Created by Aksheet Dutta on 8/2/24.
+//
+
+import SwiftUI
+
+@main
+struct UMaxAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
