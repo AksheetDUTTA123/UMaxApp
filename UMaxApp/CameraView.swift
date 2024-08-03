107675f03cac9ad37232ec78b4015cf414f00827
Aksheet Dutta <45073678+AksheetDUTTA123@users.noreply.github.com>
2024-08-03 14:27:15 -0400

    Update CameraView.swift

--- UMaxApp/CameraView.swift
+++ UMaxApp/CameraView.swift
@@ -7,74 +7,103 @@
 
 import SwiftUI
 import UIKit
-import AVFoundation
 
-struct ImagePicker : View{
-    @State private var isShown:Bool = false
-    @State private var image:Image = Image(systemName:"")
-    @State private var sourceType: UIImagePickerController.SourceType = .camera
-    var body:some View {
-        VStack{
-            image.resizable().frame(width:200, height:200)
-            Button(action: {
-                self.isShown.toggle()
-                self.sourceType = .camera
-            }){
-                Text("Camera")
-            }
-            Button(action: {
-                self.isShown.toggle()
-                self.sourceType = .photoLibrary
-            }){
-                Text("Photo Library")
-            }
-            
-        }.sheet(isPresented: $isShown){
-            A(isShown:self.$isShown, myImage : self.$image, mysourceType:self.$sourceType)
-    }
-    }
-    }
+struct IdentifiableImage: Identifiable, Hashable {
+    let id = UUID()
+    let uiImage: UIImage
 
-struct ImagePicker_Previews:PreviewProvider {
-    static var previews: some View{
-        ImagePicker()
+    func image() -> Image {
+        Image(uiImage: uiImage)
     }
 }
-struct A:UIViewControllerRepresentable{
-    @Binding var isShown : Bool
-    @Binding var myImage : Image
-    @Binding var mysourceType : UIImagePickerController.SourceType
-    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<A>) {
+
+struct ImagePicker: View {
+    @State private var isShown: Bool = false
+    @State private var images: [IdentifiableImage] = []
+    @State private var sourceType: UIImagePickerController.SourceType = .camera
+    
+    var body: some View {
+        VStack {
+            Text("Insert at most 3 pictures of your body. Make sure your entire body is visible.")
+            ScrollView{
+                
+                ForEach(images) { identifiableImage in
+                    identifiableImage.image()
+                        .resizable()
+                        .frame(width: 200, height: 200)
+                        .padding()
+                }
+            }
+            if self.images.count < 3 {
+                Button(action: {
+                    self.isShown.toggle()
+                    self.sourceType = .camera
+                    
+                }) {
+                    Text("Camera")
+                }
+                Button(action: {
+                    self.isShown.toggle()
+                    self.sourceType = .photoLibrary
+                    
+                }) {
+                    Text("Photo Library")
+                }
+            }
+        }
+        NavigationLink(destination:AnalyzeView()){
+            Text("Click for AI Reccomendations!").frame(width:300, height:200, alignment: .center).background(Color.black).foregroundColor(Color.yellow).cornerRadius(30)
+                .sheet(isPresented: $isShown) {
+                    ImagePickerView(isShown: self.$isShown, images: self.$images, sourceType: self.sourceType)
+                }
+        }
         
     }
-    func makeUIViewController(context: UIViewControllerRepresentableContext<A>) -> UIImagePickerController {
-        let obj = UIImagePickerController()
-        obj.sourceType = mysourceType
-        obj.delegate = context.coordinator
-        return obj
-    }
-    func makeCoordinator() -> C {
-        return C(isShown : $isShown, myimage :$myImage)
-    }
-}
-
-class C: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    @Binding var isShown: Bool
-    @Binding var myimage : Image
-    init(isShown: Binding<Bool>, myimage : Binding <Image>){
-        _isShown = isShown
-        _myimage = myimage
+    
+    struct ImagePicker_Previews: PreviewProvider {
+        static var previews: some View {
+            ImagePicker()
+        }
     }
     
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
-            print (image)
-            myimage = Image.init(uiImage : image)
-            
+    struct ImagePickerView: UIViewControllerRepresentable {
+        @Binding var isShown: Bool
+        @Binding var images: [IdentifiableImage]
+        var sourceType: UIImagePickerController.SourceType
+        
+        func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePickerView>) {}
+        
+        func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePickerView>) -> UIImagePickerController {
+            let picker = UIImagePickerController()
+            picker.sourceType = sourceType
+            picker.delegate = context.coordinator
+            return picker
         }
-        isShown = false
-    }
-    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        isShown = false
+        
+        func makeCoordinator() -> Coordinator {
+            Coordinator(isShown: $isShown, images: $images)
+        }
+        
+        class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+            @Binding var isShown: Bool
+            @Binding var images: [IdentifiableImage]
+            
+            init(isShown: Binding<Bool>, images: Binding<[IdentifiableImage]>) {
+                _isShown = isShown
+                _images = images
+            }
+            
+            func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+                if let uiImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+                    let identifiableImage = IdentifiableImage(uiImage: uiImage)
+                    images.append(identifiableImage)
+                }
+                isShown = false
+            }
+            
+            func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+                isShown = false
+            }
+        }
     }
 }
