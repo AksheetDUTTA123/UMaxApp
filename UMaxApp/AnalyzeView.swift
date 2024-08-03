9185eb8a90c5f53ac191ca8ffe9c3bbc1036896e
Aksheet Dutta <ardutta23@gmail.com>
2024-08-03 14:23:20 -0400

    3 picture max support currently, good to go. Working on the AI part now.

---
+++ UMaxApp/AnalyzeView.swift
@@ -0,0 +1,18 @@
+//
+//  AnalyzeView.swift
+//  UMaxApp
+//
+//  Created by Aksheet Dutta on 8/3/24.
+//
+
+import SwiftUI
+
+struct AnalyzeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnalyzeView()
+}
